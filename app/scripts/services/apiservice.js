2f273ae8efd90f9b04922a7d18890859f2ad6b3a
uf0 <scnepa222@gmail.com>
2016-02-15 18:44:25 +0100

    added apiservice

---
+++ app/scripts/services/apiservice.js
@@ -0,0 +1,70 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name anthropocenesiteApp.apiService
+ * @description
+ * # apiService
+ * Factory in the anthropocenesiteApp.
+ */
+angular.module('anthropocenesiteApp')
+  .factory('apiService', function () {
+    var baseurl = 'http://dicto-ao.herokuapp.com/api/'
+        return {
+            getEntities : function() {
+                var deferred = $q.defer();
+                $http.get(baseurl+'entities').success(function(data){
+                    deferred.resolve(data);
+                }).error(function(){
+                    deferred.reject("An error occured while fetching file");
+                });
+                return deferred.promise;
+            },
+            getEntity : function(id) {
+                var deferred = $q.defer();
+                $http.get(baseurl+'entities/'+id).success(function(data){
+                    deferred.resolve(data);
+                }).error(function(){
+                    deferred.reject("An error occured while fetching file");
+                });
+                return deferred.promise;
+            },
+            getChunk : function(id) {
+                var deferred = $q.defer();
+                $http.get(baseurl+'chunks/'+id).success(function(data){
+                    deferred.resolve(data);
+                }).error(function(){
+                    deferred.reject("An error occured while fetching file");
+                });
+                return deferred.promise;
+            },
+            getPlaylists : function() {
+                var deferred = $q.defer();
+                $http.get(baseurl+'playlists').success(function(data){
+                    deferred.resolve(data);
+                }).error(function(){
+                    deferred.reject("An error occured while fetching file");
+                });
+                return deferred.promise;
+            },
+            getPlaylist : function(id) {
+                var deferred = $q.defer();
+                $http.get(baseurl+'playlists/'+id).success(function(data){
+                    deferred.resolve(data);
+                }).error(function(){
+                    deferred.reject("An error occured while fetching file");
+                });
+                return deferred.promise;
+            },
+            getFile : function(url){
+                var deferred = $q.defer();
+                $http.get(url).success(function(data){
+                    deferred.resolve(data);
+                }).error(function(){
+                    deferred.reject("An error occured while fetching file");
+                });
+
+                return deferred.promise;
+            }
+        }
+    });
